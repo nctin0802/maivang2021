4f9c3bd51580e0fc8bec93d66bcdb011a78e2296
Tin Nguyen <nctin0802@gmail.com>
2021-02-19 11:59:12 +0700

    first-push-angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'maivang2021';
+  title = 'Calculator App';
+  num1:number = 0;
+  num2:number = 0;
+  result:number = 0;
+
+  add() {
+    this.result = this.num1 + this.num2;
+  }
+  substract() {
+    this.result = this.num1 - this.num2;
+  }
+  multiply() {
+    this.result = this.num1 * this.num2;
+  }
+  divide() {
+    this.result = this.num1 % this.num2;
+  }
 }
